ca220854b98e3e545e8057ec30c65fa417b81bae
Flynn Duniho <flynn@harveyosity.org>
2026-04-12 18:08:24 -0700

    Don't skip any notes in midi files

--- Py2Arduino/StreamFile.py
+++ Py2Arduino/StreamFile.py
@@ -12,9 +12,9 @@ def stream_file(args):
     ticks_per_beat = mid.ticks_per_beat
     tempo = 500000
 
-    all_messages = []
-
+    all_notes = []
     for track in mid.tracks:
+        all_messages = []
         for msg in track:
             if msg.type == "note_on" or msg.type == "note_off":
                 if msg.type == "note_on" and msg.velocity == 0:
@@ -25,27 +25,31 @@ def stream_file(args):
             elif msg.type == "set_tempo":
                 tempo = msg.tempo
 
-    piano = [Note(get_enum_string(i + SEMITONE_SHIFT + 12), 0, 0) for i in range(128)]
-    all_notes = []
-    total_time = 0
-    for i in range(len(all_messages)):
-        curr = all_messages[i]
-        total_time += curr.time
-        if curr.type == "note_on":
-            piano[curr.note] = Note(
-                piano[curr.note].pitch,
-                round(tick2second(total_time, ticks_per_beat, tempo) * 1_000),
-                0,
-            )
-        elif curr.type == "note_off":
-            piano[curr.note].length = round(
-                tick2second(total_time, ticks_per_beat, tempo) * 1_000
-                - piano[curr.note].time
-            )
-            all_notes.append(piano[curr.note].clone())
+        piano = [
+            Note(get_enum_string(i + SEMITONE_SHIFT + 12), 0, 0) for i in range(128)
+        ]
+        total_time = 0
+        for i in range(len(all_messages)):
+            curr = all_messages[i]
+            total_time += curr.time
+            if curr.type == "note_on":
+                piano[curr.note] = Note(
+                    piano[curr.note].pitch,
+                    round(tick2second(total_time, ticks_per_beat, tempo) * 1_000),
+                    0,
+                )
+            elif curr.type == "note_off":
+                piano[curr.note].length = round(
+                    tick2second(total_time, ticks_per_beat, tempo) * 1_000
+                    - piano[curr.note].time
+                )
+                all_notes.append(piano[curr.note].clone())
 
     all_notes = [n for n in all_notes if n.length > 0]
     all_notes = sorted(all_notes, key=lambda x: x.time)
+    first_start = all_notes[0].time
+    for note in all_notes:
+        note.time = note.time - first_start
 
     motor_notes: List[List[Note]] = [[] for _ in range(MOTOR_COUNT)]
 
@@ -61,6 +65,19 @@ def stream_file(args):
             if end < earliest_end and note.time > motor[-1].time:
                 earliest_index = i
                 earliest_end = end
+
+        if earliest_index == -1:
+            earliest_start = note.time + 99999
+            earliest_index = -1
+            for i, motor in enumerate(motor_notes):
+                if len(motor) == 0:
+                    earliest_index = i
+                    earliest_start = 0
+                    break
+                start = motor[-1].time
+                if start < earliest_start:
+                    earliest_index = i
+                    earliest_start = start
 
         if earliest_index != -1:
             early = motor_notes[earliest_index]
